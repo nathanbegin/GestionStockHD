70981d279c05e0b11098cd75f468ced1b3abae47
Nathan Begin <nathanbegin@hotmail.com>
2026-08-04 18:31:27 -0400

    Refresh PWA cache for role access and back navigation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "remplissage-v5-3";
+const CACHE = "remplissage-v5-4";
 const STATIC = ["/", "/index.html", "/styles.css", "/signup-fix.js", "/dashboard-navigation.js", "/app.js", "/manifest.webmanifest", "/icons/icon-192.png", "/icons/icon-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(STATIC)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
