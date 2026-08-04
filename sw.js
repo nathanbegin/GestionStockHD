0f70f9e192d04e1a33dd3d255f60361ea1771648
Nathan Begin <nathanbegin@hotmail.com>
2026-08-04 17:09:31 -0400

    Refresh PWA cache after signup fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "remplissage-v5";
+const CACHE = "remplissage-v5-2";
 const STATIC = ["/", "/index.html", "/styles.css", "/app.js", "/manifest.webmanifest", "/icons/icon-192.png", "/icons/icon-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(STATIC)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
