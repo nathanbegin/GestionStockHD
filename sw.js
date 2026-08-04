28f7a71fe54d353270149f60f62251c4dd943300
Nathan Begin <nathanbegin@hotmail.com>
2026-08-04 18:52:06 -0400

    Refresh PWA cache for guided article entry

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "remplissage-v5-4";
-const STATIC = ["/", "/index.html", "/styles.css", "/signup-fix.js", "/dashboard-navigation.js", "/app.js", "/manifest.webmanifest", "/icons/icon-192.png", "/icons/icon-512.png"];
+const CACHE = "remplissage-v5-5";
+const STATIC = ["/", "/index.html", "/styles.css", "/article-wizard.css", "/signup-fix.js", "/dashboard-navigation.js", "/article-wizard.js", "/app.js", "/manifest.webmanifest", "/icons/icon-192.png", "/icons/icon-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(STATIC)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
