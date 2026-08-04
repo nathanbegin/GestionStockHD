41aa5f98be974ccb6cc8c7b6059b723d9d8ad9c8
Nathan Begin <nathanbegin@hotmail.com>
2026-08-04 18:26:47 -0400

    Refresh PWA cache for dashboard navigation

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "remplissage-v5-2";
-const STATIC = ["/", "/index.html", "/styles.css", "/app.js", "/manifest.webmanifest", "/icons/icon-192.png", "/icons/icon-512.png"];
+const CACHE = "remplissage-v5-3";
+const STATIC = ["/", "/index.html", "/styles.css", "/signup-fix.js", "/dashboard-navigation.js", "/app.js", "/manifest.webmanifest", "/icons/icon-192.png", "/icons/icon-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(STATIC)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
