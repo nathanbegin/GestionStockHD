f0edfa67c9c40bedab7a8afde08dc7856cd4733f
Nathan Begin <nathanbegin@hotmail.com>
2026-08-04 22:45:35 -0400

    Refresh PWA cache for entry mode and GES panel fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "remplissage-v5-10";
+const CACHE = "remplissage-v5-11";
 const STATIC = ["/", "/index.html", "/styles.css", "/article-wizard.css", "/ges-locations.css", "/signup-fix.js", "/dashboard-navigation.js", "/ges-locations.js", "/article-wizard.js", "/pickup-today.js", "/pdf-local-time.js", "/app.js", "/manifest.webmanifest", "/icons/icon-192.png", "/icons/icon-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(STATIC)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
