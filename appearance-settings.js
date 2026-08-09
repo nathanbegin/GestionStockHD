bccc118c0fa5a1ccde700c3f0f49c084e8258b0a
Nathan Begin <nathanbegin@hotmail.com>
2026-08-08 22:36:45 -0400

    Mettre à jour la version affichée

--- appearance-settings.js
+++ appearance-settings.js
@@ -1,6 +1,6 @@
 (() => {
   const STORAGE_KEY = "restock_appearance_v1";
-  const APP_VERSION = "v5-72";
+  const APP_VERSION = "v5-73";
   const PALETTES = ["orange", "blue", "green", "purple"];
   const BRAND_COLORS = {
     orange: "#f96302",
