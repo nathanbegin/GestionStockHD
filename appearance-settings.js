345d41a33516034d24f0940ef9eae16ba621d22e
Nathan Begin <nathanbegin@hotmail.com>
2026-08-09 08:40:28 -0400

    Afficher la version v5-76

--- appearance-settings.js
+++ appearance-settings.js
@@ -1,6 +1,6 @@
 (() => {
   const STORAGE_KEY = "restock_appearance_v1";
-  const APP_VERSION = "v5-75";
+  const APP_VERSION = "v5-76";
   const PALETTES = ["orange", "blue", "green", "purple"];
   const BRAND_COLORS = {
     orange: "#f96302",
