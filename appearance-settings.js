b39dba940c76a2c4e39a393aeedd1bb551eaa04f
Nathan Begin <nathanbegin@hotmail.com>
2026-08-10 14:27:09 -0400

    Passer l’application en v5-84

--- appearance-settings.js
+++ appearance-settings.js
@@ -1,6 +1,6 @@
 (() => {
   const STORAGE_KEY = "restock_appearance_v1";
-  const APP_VERSION = "v5-83";
+  const APP_VERSION = "v5-84";
   const PALETTES = ["orange", "blue", "green", "purple"];
   const BRAND_COLORS = {
     orange: "#f96302",
